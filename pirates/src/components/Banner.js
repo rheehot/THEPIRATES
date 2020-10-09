4334dfae3e2338e95036d73cd475308319b290fe
spring1826 <dongwon1826@gmail.com>
2020-10-09 14:58:21 +0900

    :sparkles:           feat    : add Banner

--- pirates/src/components/Banner.js
+++ pirates/src/components/Banner.js
@@ -1,26 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
-
+import BannerData from "../Data/bannerData.json";
+// <i class="fas faChevronDown"></i>
 const Container = styled.div``;
 
-const List = styled.ul``;
+const List = styled.ul`
+  position: relative;
+`;
 
 const Item = styled.li`
   width: 100%;
   height: 41px;
   background-color: #313841;
+  color: white;
+  font-size: 15px;
+  text-align: center;
+  line-height: 2.7;
 `;
 
-const ToggleBtn = styled.div``;
+const BannerLabel = styled.span``;
 
-const Banner = () => (
-  <Container>
-    <List>
-      <Item></Item>
-      <Item></Item>
-      <ToggleBtn>btn</ToggleBtn>
-    </List>
-  </Container>
-);
+const BannerPrice = styled.span`
+  color: #b8e986;
+`;
+
+const ToggleBtn = styled.div`
+  color: white;
+  font-size: 14px;
+  position: absolute;
+  top: 13px;
+  right: 10px;
+  cursor: pointer;
+`;
+let count = null;
+
+const Banner = () => {
+  const [toggle, setToggle] = useState(false);
+  const [mainData, setMainData] = useState(BannerData[0]);
+
+  useEffect(() => {
+    if (count === null) {
+      count = 1;
+    }
+    setTimeout(() => {
+      if (BannerData.length > count) {
+        setMainData(BannerData[count]);
+      }
+      if (BannerData.length - 1 === count) {
+        count = 0;
+      } else {
+        count++;
+      }
+    }, 2000);
+  }, [mainData]);
+
+  return (
+    <Container>
+      {toggle ? (
+        <List>
+          {BannerData.map((item, i) => (
+            <Item key={i}>
+              <BannerLabel>{`${item.zoneLabel}: ${item.label} `}</BannerLabel>
+              <BannerPrice>{item.price}</BannerPrice>
+              <BannerLabel>{` (${item.comment})`}</BannerLabel>
+            </Item>
+          ))}
+          <ToggleBtn
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronDown} rotation={180} />
+          </ToggleBtn>
+        </List>
+      ) : (
+        <List>
+          <Item>
+            <BannerLabel>{`${mainData.zoneLabel}: ${mainData.label} `}</BannerLabel>
+            <BannerPrice>{mainData.price}</BannerPrice>
+            <BannerLabel>{` (${mainData.comment})`}</BannerLabel>
+          </Item>
+          <ToggleBtn
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronDown} />
+          </ToggleBtn>
+        </List>
+      )}
+    </Container>
+  );
+};
 
 export default Banner;
